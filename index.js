50a70e1920b2b866598e3a180da871561ea2eaca
Shouvik Banerjee <banerjeeshouvik29@gmail.com>
2024-10-19 00:05:23 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+function table_book(){
+    open('table.html')
+}
+function statare_click(){
+    document.getElementById('e1').style.backgroundImage ="url('chop.jpeg.jpg')";
+    document.getElementById('e2').style.backgroundImage ="url('Burger.jpg')";
+    document.getElementById('e3').style.backgroundImage ="url('s1.jpeg')";
+    document.getElementById('e4').style.backgroundImage ="url('s2.jpeg')";
+    document.getElementById('e5').style.backgroundImage ="url('s3.jpeg')";
+    document.getElementById('e6').style.backgroundImage ="url('s4.jpeg')";
+    document.getElementById('e7').style.backgroundImage ="url('s5.jpeg')";
+    document.getElementById('e8').style.backgroundImage ="url('s6.jpeg')";
+}
+function drink_click(){
+    document.getElementById('e1').style.backgroundImage ="url('mock1.jpg')";
+    document.getElementById('e2').style.backgroundImage ="url('mock2.jpg')";
+    document.getElementById('e3').style.backgroundImage ="url('mock3.jpg')";
+    document.getElementById('e4').style.backgroundImage ="url('mock4.jpg')";
+    document.getElementById('e5').style.backgroundImage ="url('tea.jpg')";
+    document.getElementById('e6').style.backgroundImage ="url('tea2.jpg')";
+    document.getElementById('e7').style.backgroundImage ="url('tea3.jpg')";
+    document.getElementById('e8').style.backgroundImage ="url('tea4.jpg')";
+}
+function dessart_click(){
+    document.getElementById('e1').style.backgroundImage ="url('d1.jpg')";
+    document.getElementById('e2').style.backgroundImage ="url('d2.jpg')";
+    document.getElementById('e3').style.backgroundImage ="url('d3.jpg')";
+    document.getElementById('e4').style.backgroundImage ="url('d4.jpg')";
+    document.getElementById('e5').style.backgroundImage ="url('d5.jpg')";
+    document.getElementById('e6').style.backgroundImage ="url('d6.jpg')";
+    document.getElementById('e7').style.backgroundImage ="url('d7.jpeg')";
+    document.getElementById('e8').style.backgroundImage ="url('d8.jpeg')";
+}
+function online(){
+    open('online.html');
+   
+}
+function contact()
+{
+    open("#container");
+}
